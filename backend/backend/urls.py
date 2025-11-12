e0b339907e7473f52c26ff8d6b4cc3712d3a694f
Titint8 <anjoriatiphanie@gmail.com>
2025-11-12 21:01:25 +0100

    test17

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("Bienvenue sur l'API de Spoon Consulting"))
+    path('', lambda request: HttpResponse("Bienvenue sur l'API de Spoon Consulting")),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/import/', include('import.urls')),
